dc2bfc63cd6cf9cd538e9c30c63ee580bbb65fad
Daniel Milkiewicz <daniel.milkiewicz@outlook.com>
2017-11-19 15:47:32 +0100

    Optimaliztion

--- js/hamburger.js
+++
@@ -1,6 +0,0 @@
-var hamburger = document.querySelector(".hamburger");
-var ul = document.querySelector(".nav-list");
-hamburger.addEventListener("click", function() {
-  hamburger.classList.toggle("is-active");
-  ul.classList.toggle("is-visible");
-});
